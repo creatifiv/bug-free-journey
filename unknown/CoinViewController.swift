0250a4849449cb743688a95fd54b5b5ccaa7a010
creatifiv <ios15dev@gmail.com>
2022-09-23 14:05:04 +0000

    Coin side label added

--- unknown/CoinViewController.swift
+++ unknown/CoinViewController.swift
@@ -13,17 +13,21 @@ class CoinViewController: UIViewController, CoinDelegate{
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         image.image = UIImage(named: "img2.png")
+        coinSide.text = " "
     }
     
-    var images = ["head2.png", "tails2.png"]
+    var images = ["us_front.png", "us_back.png"]
     
     var i = 0
+    var obverseCount = 0
+    var reverseCount = 0
     var coinFlipped = false
     
 
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var status: UILabel!
     @IBOutlet weak var coinButton: UIButton!
+    @IBOutlet weak var coinSide: UILabel!
     
     //MARK: Select new coin view controller
     @IBAction func changeCoin(_ sender: UIButton) {
@@ -41,6 +45,27 @@ class CoinViewController: UIViewController, CoinDelegate{
     @IBAction func flipCoin(_ sender: Any) {
         i = Int.random(in: 0...1)
         changeImage()
+        
+        if i == 0{
+            coinSide.text = "Heads"
+            obverseCount += 1
+            if obverseCount > 1{
+                coinSide.text! += " " + "x" + String(obverseCount)
+            }
+        }
+        else{
+            obverseCount = 0
+        }
+        if i == 1{
+            coinSide.text = "Tails"
+            reverseCount += 1
+            if reverseCount > 1{
+                coinSide.text! +=  " " + "x" + String(reverseCount)
+            }
+        }
+        else{
+            reverseCount = 0
+        }
     }
     
     func getCoin(coin: Coin) {
